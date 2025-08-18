3a0bd660ed10a93516e948b0952fb9c710cfd565
sitisyarafana <sitisyarafana07@gmail.com>
2025-08-18 12:06:01 +0800

    ble

---
+++ labs/lab06/lab-test-2.py
@@ -0,0 +1,5 @@
+#name Siti Syarafana Binti Sabdin
+#I have to make an algorithm based on output given by using appropriate escape characters and arithmetic expression
+lab = "Name: Ahmad Bin Abu\t\t\t\t\tMatric. No: MS2025123499\t\t\t\n\n*\t\t\t*\n**\t\t**\n***\t***\n********\n***\t***\n**\t\t**\n*\t\t\t*\n\n\nThis is my\n\tsecond\n\t\tassignment\nI want 2x10 marks, which is 20 full marks"
+
+print(lab)
