7afe797a45888f6cc3e4ccf2c5a0197ba99fe23e
sitisyarafana <sitisyarafana07@gmail.com>
2025-09-22 11:31:40 +0800

    e

--- labs/lab09/exercise2/exercise2.py
+++ labs/lab09/exercise2/exercise2.py
@@ -1,33 +1,41 @@
 employee_name = input()
-base_salary = float(input())
+base_salary  = float(input())
 overtime_hours = int(input())
 tax_status = input()
 
 # TODO your code here
+overtime_pay = overtime_hours * 35
+total_income = base_salary  + overtime_pay
+
 if (tax_status == Single):
-    if (base_salary >= 5000):
-       tax = 22
-       tax_rate = "22%"
+    if ( total_income >= 5000):
+       tax_rate = 0.22
+       tax = "22%"
     else :
-       tax = 18
-       tax_rate = "18%"
+       tax_rate = 0.18
+       tax = "18%"
 elif (tax_status == Married):
-    if (base_salary >= 6000):
-       tax = 20
-       tax_rate = "20%"
+    if (total_income  >= 6000):
+       tax_rate = 0.20
+       tax = "20%"
     else :
-       tax = 25
-       tax_rate = "15%"
+       tax_rate = 0.25
+       tax = "15%"
 else:
-    if (base_salary >= 5500):
-       tax = 25
-       tax_rate = "25%"
+    if (total_income  >= 5500):
+       tax_rate = 0.25
+       tax = "25%"
     else :
-       tax = 19
-       tax_rate = "19%"
+       tax_rate = 0.19
+       tax = "19%"
 
-net_salary = base_salary * (tax/100)
+tax_amount = total_income * tax_rate
+epf = total_income * 0.11
+socso = total_income * 0.005
 
+net_salary = total_income - tax_amount - epf - socso
+
+net_salary = total_income  * (tax/100)
 
 print(employee_name)
 print(tax_rate)
--- labs/lab09/exercise4/exercise4.py
+++ labs/lab09/exercise4/exercise4.py
@@ -3,6 +3,21 @@ previous_reading = int(input())
 
 # TODO: Your code here
 
+cubic_meter = int(input("Enter cubic meter"))
+
+# rate structure
+if cubic_meter <= 20:
+    consumption = cubic_meter * 0.57
+elif cubic_meter <= 35:
+    consumption = cubic_meter * 1.03
+else :
+    consumption = cubic_meter * 1.40
+
+# calculate water cost
+water_cost = consumption * (current_reading + previous_reading)
+
+# calculate total bill
+total_bill = water_cost + 8.00 + 2.00
 
 print(consumption)
 print(water_cost)
