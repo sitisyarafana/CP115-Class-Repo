b130c7130c0a89453f520102dee3de119293c95b
sitisyarafana <sitisyarafana07@gmail.com>
2025-10-06 13:02:44 +0800

    sir

--- labs/lab12/exercise2/exercise2.py
+++ labs/lab12/exercise2/exercise2.py
@@ -1,6 +1,19 @@
+score_count = 0
+total_score = 0
+average_score = 0.0
+
 score = float(input())
 
 # TODO: Your code here
+while 0 <= score <= 100:
+    total_score += score
+    score_count += 1
+    score = float(input())
+
+if score_count > 0:
+    average_score = total_score / score_count
+else:
+    average_score = 0.0
 
 
 print(score_count)
--- labs/lab12/exercise3/exercise3.py
+++ labs/lab12/exercise3/exercise3.py
@@ -1,6 +1,21 @@
+age_count = 0
+total_age = 0
+average_age = 0.0
+
 age_input = input()
 
 # TODO: Your code here
+while age_input.lower() != "done" :
+    age = int(age_input)
+    age_count += 1
+    total_age += age
+    age_input = input()
+
+if age_count > 0:
+    average_age = total_age/age_count
+else:
+    average_age = 0.0
+
 
 print(age_count)
 print(total_age)
